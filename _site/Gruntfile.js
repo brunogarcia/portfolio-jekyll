c8ee381452c76a5bd21d45d0dcd37b346b782bf8
Bruno Garcia <bruno@garciaechegaray.com>
2014-03-16 18:22:38 +0100

    Config Bower and Grunt

---
+++ _site/Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+
+    // 1. All configuration goes here 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {   
+            dist: {
+                src: [
+                    'bower_components/jquery/jquery.js',
+                    'bower_components/foundation/js/foundation.js',
+                    'js/app.js'                    
+                ],
+                dest: 'js/build/production.js',
+            }
+        },
+
+        uglify: {
+            build: {
+                src: 'js/build/production.js',
+                dest: 'js/build/production.min.js'
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ['js/*.js'],
+                tasks: ['concat', 'uglify'],
+                options: {
+                    spawn: false,
+                },
+            } 
+        }                
+
+    });
+
+    // 3. Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('default', ['concat', 'uglify', 'watch']);
+
+};
